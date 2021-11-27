bf2f9f0008c4c26b5964c321243c9e9e7f55a28b
allin17 <zhernosek.zahar@mail.ru>
2021-11-27 07:43:51 +0300

    router pages

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ let AppContainer = compose(
 
 
 const SamuraiJSApp = (props) => {
-    return <BrowserRouter>
+    return <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <AppContainer />
         </Provider>
